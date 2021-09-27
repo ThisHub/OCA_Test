fc0b748b943cd026d1a8878cbce081f91f39d217
ThisHub <jenspieter.mievis@student.intecbrussel.be>
2021-09-27 21:36:35 +0200

    main basically finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,8 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+
+
     public static void main(String[] args) {
         Questions questions = new Questions();
         Answers answers = new Answers();
@@ -28,6 +30,31 @@ public class Main {
              System.out.println("Please enter a valid choice.");
         }
 
+        while (go.toLowerCase(Locale.ROOT).equals("y")){
+            int questionNo = random.nextInt(199);
+            System.out.println(questions.questions.get(questionNo));
+            System.out.print("Your answer: ");
+            String answer = scanner.next();
+            System.out.println(answer);
+            if (answer.toLowerCase(Locale.ROOT).equals(answers.answers.get(questionNo).toLowerCase(Locale.ROOT))){
+                System.out.println("Correct!, here is why: " + explanation.explanations.get(questionNo));
+            }
+            else
+                System.out.println("Incorrect. Better luck next time here is the explanation: \n" + explanation.explanations.get(questionNo));
+            System.out.println("Press enter to continue, press C to stop");
+            while (!go.toLowerCase(Locale.ROOT).equals("")||!go.toLowerCase(Locale.ROOT).equals("c")){
+                go = scanner.next();
+                if (go.toLowerCase(Locale.ROOT).equals("")||go.toLowerCase(Locale.ROOT).equals("n")) {
+                    if (go.toLowerCase(Locale.ROOT).equals(""))
+                        go = "y";
+                    break;
+                }
+                else
+                    System.out.println("Please enter a valid choice.");
+            }
+
+        }
+
 //        for (String question : questions.questions) {
 //            System.out.println(question +"\n");
 //
@@ -37,5 +64,9 @@ public class Main {
 //            System.out.println(answer + "\n");
 //        }
 
+    }
+
+    public void getQuestion(int id){
+        ;
     }
 }
